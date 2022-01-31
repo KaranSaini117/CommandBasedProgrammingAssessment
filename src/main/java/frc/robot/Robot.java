097521bf171dc11284c24997d496e10f349c3e41
Karan Saini <karansaini117@gmail.com>
2022-01-31 00:28:29 -0800

    initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,59 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+  private RobotContainer robotContainer;
+
+  @Override
+  public void robotInit() {
+    robotContainer = new RobotContainer();
+  }
+
+  
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  @Override
+  public void autonomousInit() {
+    robotContainer.getAutonomousCommand().schedule();
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+  
+  @Override
+  public void teleopInit() {
+    robotContainer.getAutonomousCommand().cancel();
+    robotContainer.setDefaultCommands();
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    CommandScheduler.getInstance().run();
+
+  }
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+}
