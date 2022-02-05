942aa5fe755454e551e8d4d1b8dfcc30ab489b66
poluk sharma <poluk.sharma@gmail.com>
2022-02-04 23:54:15 -0800

    no message

---
+++ src/main/java/frc/robot/subsystems/cargo/RunIntake.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.hatch;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class RunIntake extends CommandBase {
+
+	public RunIntake(IntakeSubsystem intakeSubsystem) {
+      this.intakeSubsystem = intakeSubsystem; 
+      this.xboxController = xboxcontroller;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intakeSubsystem, xboxcontroller);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+      if(xboxController.getBButton{}
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
